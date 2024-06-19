8c3d26b1a8727a7137af61ca1aa0f1f894c9ad1c
jonas.ge <jonas.ge@jonas2ge.local>
2024-06-20 01:39:19 +0800

    feat: add new version & eslint

--- packages/cus-loader-utils/lib/beforeExitHook.ts
+++ packages/cus-loader-utils/lib/beforeExitHook.ts
@@ -4,8 +4,8 @@ type TypeBeforeExitHook = (cb: () => void) => void;
 const beforeExitHook: TypeBeforeExitHook = (cb) => {
     // 执行结束，释放内存
     process.on('beforeExit', function () {
-        console.log('执行结束，释放内存');
         cb();
+        console.log('执行结束，释放内存');
     });
 };
 
